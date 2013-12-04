05b403474044a013bb3b9f38209525af5b9af379
Piotr Bagazja <bagieta@gmail.com>
2013-12-04 11:44:22 +0100

    Pierwsza wbita

---
+++ Enums/Enums.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Enums.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.ComponentModel;
+
+public enum EVisibility
+{ 
+    Public = 0,
+    Unlisted =1,
+    Private = 2
+}
+public enum ExpireData
+{ 
+    [Description("N")]
+    Never, 
+    [Description("10M")]
+    tenMin,
+[Description("1H")] 
+    Hour, 
+[Description("1D")]
+    Day, 
+[Description("1W")]
+    oneWeek, 
+[Description("2W")]
+    twoWeek, 
+[Description("1M")]
+    Month 
+}
