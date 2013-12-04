bbdf47e45cd0c30e79a0d6665a08db0e8885f416
Piotr Bagazja <bagieta@gmail.com>
2013-12-04 12:10:43 +0100

    Dodanie plikoów

---
+++ PasteBin/Enums/EnumHelper.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="EnumHelper.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace PasteBin
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.ComponentModel;
+    using System.Reflection;
+
+    public static class EnumHelper
+    {
+        public static string GetDescription(this Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute attribute
+                    = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute))
+                        as DescriptionAttribute;
+
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+    }
+}
